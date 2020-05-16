14c6cb11a1ac3eafa11da8e24d6a3715578e9f63
Aleksanderprofic <aleprofic@gmail.com>
2020-05-16 14:31:12 +0200

    Added solution and README

---
+++ LeetCode/Interview questions/Easy/Strings/1_879_Reverse_String/solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    string = ['h', 'a', 'n', 's']
+    sol.reverseString(string)
+    print(string)
