00fb5519de916468635587e9732e3d382b89026f
Aleksanderprofic <aleprofic@gmail.com>
2020-05-17 10:22:20 +0200

    Added solution using Counter for 674

--- LeetCode/Interview questions/Easy/Array/6__674_Intersection_of_Two_Arrays_II/solution.py
+++ LeetCode/Interview questions/Easy/Array/6__674_Intersection_of_Two_Arrays_II/solution.py
@@ -1,4 +1,5 @@
 from typing import List
+from collections import Counter
 
 
 class Solution:
@@ -18,7 +19,21 @@ class Solution:
 
         return list_to_return
 
+    def intersect_using_counter(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        count = Counter(nums1)
+        list_to_return = []
+
+        for num in nums2:
+            if count[num]:
+                list_to_return.append(num)
+                count[num] -= 1
+
+        return list_to_return
+
 
 sol = Solution()
-print(sol.intersect(nums1=[1, 2, 2, 1], nums2=[2, 2]))
-print(sol.intersect(nums1=[4, 9, 5], nums2=[9, 4, 9, 8, 4]))
+print(sol.intersect_using_counter(nums1=[1, 2, 2, 1], nums2=[2, 2]))
+print(sol.intersect_using_counter(nums1=[4, 9, 5], nums2=[9, 4, 9, 8, 4]))
+
+cnt = Counter([1, 2, 5, 2, 2, 1, 5, 7])
+print()
