c3cf845ab3a8b7371658f3cd8d096b243ae329a3
Aleksanderprofic <aleprofic@gmail.com>
2020-05-17 10:51:22 +0200

    Added solution for 882

---
+++ LeetCode/Interview questions/Easy/Strings/4_882_Valid_Anagram/solution.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+
+
+sol = Solution()
+print(sol.isAnagram(s="anagram", t="nagaram"))
+print(sol.isAnagram(s="rat", t="car"))
